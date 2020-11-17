f7ffe67853b4889a5c83aec25fbf3eef6bffd1e1
Maxsuel Aparecido Lima Santos <maxsuel.ap.l.santos@gmail.com>
2020-11-17 15:09:43 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,10 @@
-let start = document.getElementById('start')
+let mascote = document.getElementById('mascote')
+mascote.addEventListener('click',Clicar)
+
+function Clicar() {
+    window.alert('Esse é o mascote do jogo. HEHE')
+}
 
 function Start() {
-    window.alert('START')
+    window.alert('O jogo começou! HEHE')
 }
